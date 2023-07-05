c4012749695872544cbb211d084ce812f4cb085a
Shivam Pandey <pandeys@microsoft.com>
2023-07-06 00:28:45 +0530

    Trying to call MFCreateSensorActivityMonitor in .NET

---
+++ MainPage.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPAPPSampleCallingCameraDLL
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Import the MFCreateSensorActivityMonitor function using P/Invoke
+        [DllImport("mfsensorgroup.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern HRESULT MFCreateSensorActivityMonitor(
+            IMFSensorActivitiesReportCallback pCallback,
+            out IMFSensorActivityMonitor ppActivityMonitor
+        );
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            IMFSensorActivitiesReportCallback callback = null;
+            IMFSensorActivityMonitor activityMonitor = null;
+
+            try
+            {
+                // Create an instance of your IMFSensorActivitiesReportCallback implementation
+                callback = new MySensorActivitiesReportCallback();
+
+                HRESULT result = MFCreateSensorActivityMonitor(callback, out activityMonitor);
+
+                int x = Marshal.GetLastWin32Error();
+
+                if (result.Equals(HRESULT.S_OK) && activityMonitor != null)
+                {
+                    Console.WriteLine("Sensor activity monitor created successfully!");
+
+                    // You can perform additional operations with the activity monitor here
+
+                    // Remember to release the activity monitor when you're done
+                    Marshal.ReleaseComObject(activityMonitor);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to create sensor activity monitor!");
+                }
+            }
+            catch (COMException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                // Release the callback object if it was created
+                if (callback != null)
+                {
+                    Marshal.ReleaseComObject(callback);
+                }
+            }
+        }
+
+        // Define the IMFSensorActivityMonitor interface
+        [ComImport]
+        [Guid("D0CEF145-B3F4-4340-A2E5-7A5080CA05CB")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMFSensorActivityMonitor
+        {
+            // Define the methods of IMFSensorActivityMonitor here
+            int Start();
+            int Stop();
+        }
+
+        // Implement your own IMFSensorActivitiesReportCallback class
+        // that provides the necessary callback functionality
+        private class MySensorActivitiesReportCallback : IMFSensorActivitiesReportCallback
+        {
+            public HRESULT OnActivitiesReport(IMFSensorActivitiesReport sensorActivitiesReport)
+            {
+                // Implement the callback method to handle the sensor activities report
+                // ...
+
+                return HRESULT.S_OK;
+            }
+        }
+
+        // Define the IMFSensorActivitiesReportCallback interface
+        [ComImport]
+        [Guid("DE5072EE-DBE3-46DC-8A87-B6F631194751")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMFSensorActivitiesReportCallback
+        {
+            HRESULT OnActivitiesReport(IMFSensorActivitiesReport sensorActivitiesReport);
+        }
+
+        // Define the IMFSensorActivitiesReport interface
+        [ComImport]
+        [Guid("683F7A5E-4A19-43CD-B1A9-DBF4AB3F7777")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMFSensorActivitiesReport
+        {
+            HRESULT GetCount(out uint pcCount);
+
+            HRESULT GetActivityReport(uint Index, out IMFSensorActivityReport sensorActivityReport);
+
+            HRESULT GetActivityReportByDeviceName(string SymbolicName, out IMFSensorActivityReport sensorActivityReport);
+        }
+
+        // Define the IMFSensorActivityReport interface
+        [ComImport]
+        [Guid("3E8C4BE1-A8C2-4528-90DE-2851BDE5FEAD")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMFSensorActivityReport
+        {
+            HRESULT GetFriendlyName([MarshalAs(UnmanagedType.LPWStr)] string FriendlyName, uint cchFriendlyName, out uint pcchWritten);
+
+            HRESULT GetSymbolicLink([MarshalAs(UnmanagedType.LPWStr)] string SymbolicLink, uint cchSymbolicLink, out uint pcchWritten);
+
+            HRESULT GetProcessCount(out uint pcCount);
+
+            HRESULT GetProcessActivity(uint Index, out IMFSensorProcessActivity ppProcessActivity);
+        }
+
+        // Define the IMFSensorProcessActivity interface
+        [ComImport]
+        [Guid("39DC7F4A-B141-4719-813C-A7F46162A2B8")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMFSensorProcessActivity
+        {
+            HRESULT GetProcessId(out uint pPID);
+
+            HRESULT GetStreamingState(out bool pfStreaming);
+
+            HRESULT GetStreamingMode(out MFSensorDeviceMode pMode);
+
+            HRESULT GetReportTime(out FILETIME pft);
+        }
+
+        // Define the MFSensorDeviceMode enum
+        private enum MFSensorDeviceMode
+        {
+            MFSensorDeviceMode_Controller = 0,
+            MFSensorDeviceMode_Shared = (MFSensorDeviceMode_Controller + 1)
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FILETIME
+        {
+            public uint dwLowDateTime;
+            public uint dwHighDateTime;
+        }
+
+        // Define the HRESULT struct for COM interop
+        [StructLayout(LayoutKind.Sequential)]
+        private struct HRESULT
+        {
+            public int Value;
+
+            public static readonly HRESULT S_OK = new HRESULT { Value = 0 };
+        }
+    }
+}
